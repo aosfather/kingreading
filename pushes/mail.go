0b936e6ede4fa46fccfed487e14720c80134ac8b
xiongxiaopeng <xiongxiaopeng@ehomepay.com.cn>
2019-10-14 16:11:20 +0800

    add muti attach

--- pushes/mail.go
+++ pushes/mail.go
@@ -5,7 +5,7 @@ import (
 	"log"
 )
 
-func SendMail(host string, port int, from, to, subject, pwd string, body string, filename string) {
+func SendMail(host string, port int, from, to, subject, pwd string, body string, filename ...string) {
 	m := gomail.NewMessage()
 	m.SetAddressHeader("From", from /*"发件人地址"*/, "发件人") // 发件人
 	m.SetHeader("To", m.FormatAddress(to, "收件人"))       // 收件人
@@ -13,8 +13,11 @@ func SendMail(host string, port int, from, to, subject, pwd string, body string,
 
 	//m.SetBody("text/html",xxxxx ") // 可以放html..还有其他的
 	m.SetBody("text/plain", body) // 正文
-	if filename != "" {
-		m.Attach(filename) //添加附件
+	if len(filename) > 0 {
+		for _, f := range filename {
+			m.Attach(f) //添加附件
+		}
+
 	}
 
 	d := gomail.NewDialer(host, port, from, pwd) // 发送邮件服务器、端口、发件人账号、发件人密码
