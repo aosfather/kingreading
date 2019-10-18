ab16eb0c59aeda179054ed0f139dba561aa83478
xiongxiaopeng <xiongxiaopeng@ehomepay.com.cn>
2019-10-18 10:32:58 +0800

    finish push and send mail

--- pushes/mail_test.go
+++ pushes/mail_test.go
@@ -3,5 +3,23 @@ package pushes
 import "testing"
 
 func TestSendMail(t *testing.T) {
-	SendMail("smtp.qq.com", 465, "xiongxiaopeng@ehomepay.com.cn", "aosfather@kindle.cn", "书", "123456", "test", "")
+	SendMail("smtp.163.com", 25, "?@163.com", "?@kindle.cn", "书", "?", "万古神帝第2446章.txt", "d:/D/2436.txt")
+}
+
+func TestSmtpClient_SendMessage(t *testing.T) {
+	sc := SmtpClient{}
+	sc.Host = "smtp.163.com"
+	sc.User = "?@163.com"
+	sc.Pwd = "?"
+	sc.Port = 25
+
+	msg := EmailMessage{}
+	msg.From = sc.User
+	msg.To = []string{"?"}
+	msg.Subject = "网络书"
+	msg.Body = "万古神帝第2445章"
+	msg.AddAttachment(Attachment{CT_TEXT, "万古神帝第2445章.txt", "d:/D/2435.txt"})
+
+	sc.SendMessage(&msg)
+
 }
